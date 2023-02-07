10075989495f3b264d4a6afbada99f70f5658a50
kallyas <kallyasmedia@gmail.com>
2023-02-07 16:01:36 +0300

    ch: create svg icons

---
+++ src/components/icons/BackArrow.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const BackArrow = ({ svgStyle = "w-6 h-6 icon-16 mr-5" }) => {
+  return (
+    <svg
+      className={svgStyle}
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M10 19l-7-7m0 0l7-7m-7 7h18"
+      ></path>
+    </svg>
+  );
+};
+
+export default BackArrow;
