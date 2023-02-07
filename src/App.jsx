6125dcc3a20a82c290247755b0385cee647cfdeb
kallyas <kallyasmedia@gmail.com>
2023-02-07 16:03:12 +0300

    ch: add new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { createBrowserRouter as Router, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
+import Contact from "./pages/Contact";
+import ErrorPage from "./pages/error-page";
 
 function App() {
 
   const routes = Router([
     {
       path: "/",
-      element: <Layout>App</Layout>
+      element: <Layout>App</Layout>,
+      errorElement: <ErrorPage />
+      
     },
+    {
+      path: "contact",
+      element: <Contact />,
+      // errorElement: <ErrorPage />
+    }
   ]);
 
   return (
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import { Link } from "react-router-dom";
+import ScrollToTop from "react-scroll-up";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
     <>
-    <Navbar />
-    {children}
+      <Navbar />
+      <main className="main">{children}</main>
+      <Footer />
+      <ScrollToTop style={scrollUpStyle} showUnder={160}>
+        <i className="fi-rr-arrow-small-up"></i>
+      </ScrollToTop>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export const scrollUpStyle = {
+  width: "56px",
+  height: "56px",
+  color: "#fff",
+  right: "20px",
+  bottom: "20px",
+  borderRadius: "8px",
+  textAlign: "center",
+  textDecoration: "none",
+  overflow: "hidden",
+  zIndex: "999 !important",
+  border: "0",
+  backgroundColor: "#fff",
+  transitionDuration: "0.2s",
+  backgroundColor: "#024430",
+};
+
+export default Layout;
