1dff7bdd530480e7c74fca157fb2c77934b52b2c
kallyas <kallyasmedia@gmail.com>
2023-06-02 09:55:25 +0300

    ch: add testimonials data

---
+++ src/data/testimonials.js
@@ -0,0 +1,18 @@
+export const testimonials = [
+  {
+    author: "Max",
+    company: "Cabbily",
+    content:
+      "Qiribu has been a game-changer for us. We were able to attract top talent to our growing team. Our employees are less stressed and motivated to drive for success. We are excited to use Qiribu as our team continues to grow.",
+    rating: 5,
+    date: "29 May 2022",
+  },
+  {
+    author: "Dan",
+    company: "Pizza Baby",
+    content:
+      "Qiribu is an absolute lifesaver for us.Our crew can grab their dough right away. It's made everyone happier and more secure.",
+    rating: 4,
+    date: "29 November 2022",
+  },
+];
