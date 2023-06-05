d08e8c0c5fa722e6863350d7846f09dfe73df5d3
kallyas <kallyasmedia@gmail.com>
2023-06-05 09:45:07 +0300

    ch: create website config

---
+++ src/data/index.js
@@ -0,0 +1,9 @@
+export const websiteConfig = {
+  homePage: {
+    heroTitle: "Take care of your workforce while taking care of business",
+    heroSubtitle:
+      "Unlock earned wage access, streamlined payroll processes, and a comprehensive HR dashboard with Qiribu. Enhance employee satisfaction and save time to prioritise business growth. Explore our solutions now for a seamless employee experience.",
+  },
+  aboutPage: {},
+  contactPage: {},
+};
