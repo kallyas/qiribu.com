6dc8903f3f322ecd10ed1efae75c78491b7bfe7d
kallyas <kallyasmedia@gmail.com>
2023-06-30 08:43:43 +0300

    fix: mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(!false);
   const menu = [
     {
       name: "Home",
@@ -42,55 +43,129 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className={`header sticky-bar ${isSticky ? "stick" : ""}`}>
-      <div className="container">
-        <div className="main-header">
-          <div className="header-left">
-            <div className="header-logo">
-              <Link className="d-flex" to="/">
+    <>
+      <div className={`header sticky-bar ${isSticky ? "stick" : ""}`}>
+        <div className="container">
+          <div className="main-header">
+            <div className="header-left">
+              <div className="header-logo">
+                <Link className="d-flex" to="/">
+                  <img
+                    src="logo_text_coral.svg"
+                    sizes="(max-width: 479px) 100vw, 479px"
+                    style={{ width: "100px" }}
+                    alt="logo"
+                  />
+                </Link>
+              </div>
+              <div className="header-nav">
+                <nav className="nav-main-menu d-none d-xl-block">
+                  <ul className="main-menu">
+                    {menu.map((item, index) =>
+                      item.hasChildren ? (
+                        <li key={index} className="has-children">
+                          <Link to={item.link}>{item.name}</Link>
+                          <ul className="sub-menu">
+                            {item.children.map((child, index) => (
+                              <li key={index}>
+                                <Link to={child.link}>{child.name}</Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
+                      ) : (
+                        <li key={index}>
+                          <Link to={item.link}>{item.name}</Link>
+                        </li>
+                      )
+                    )}
+                  </ul>
+                </nav>
+                <div
+                  className={`burger-icon burger-icon-white ${toggle ? "burger-close" : ""}`}
+                  onClick={() => setToggle(!toggle)}
+                >
+                  <span className="burger-icon-top"></span>
+                  <span className="burger-icon-mid"></span>
+                  <span className="burger-icon-bottom"></span>
+                </div>
+              </div>
+              <div className="header-right">
+                <div className="d-none d-sm-inline-block">
+                  <Link className="btn btn-brand-1 hover-up" to="/contact">
+                    Request Demo
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`mobile-header-active mobile-header-wrapper-style perfect-scrollbar ${
+          toggle ? "sidebar-visible" : ""
+        }`}
+      >
+        <div className="mobile-header-wrapper-inner ps ps--active-y">
+          <div className="mobile-header-content-area">
+            <div className="mobile-logo">
+              <a className="d-flex" href="/">
                 <img
                   src="logo_text_coral.svg"
                   sizes="(max-width: 479px) 100vw, 479px"
                   style={{ width: "100px" }}
                   alt="logo"
                 />
-              </Link>
+              </a>
             </div>
-            <div className="header-nav">
-              <nav className="nav-main-menu d-none d-xl-block">
-                <ul className="main-menu">
-                  {menu.map((item, index) =>
-                    item.hasChildren ? (
-                      <li key={index} className="has-children">
-                        <Link to={item.link}>{item.name}</Link>
-                        <ul className="sub-menu">
-                          {item.children.map((child, index) => (
-                            <li key={index}>
-                              <Link to={child.link}>{child.name}</Link>
+            <div
+              onClick={() => setToggle(!toggle)}
+              className={`burger-icon ${toggle ? "burger-close" : ""}`}
+            >
+              <span className="burger-icon-top"></span>
+              <span className="burger-icon-mid"></span>
+              <span className="burger-icon-bottom"></span>
+            </div>
+            <div className="perfect-scroll">
+              <div className="mobile-menu-wrap mobile-header-border">
+                <div className="tab-content">
+                  <div className="tab-pane fade active show" id="mobile-menu">
+                    <nav className="mt-15">
+                      <ul className="mobile-menu font-heading">
+                        {menu.map((item, index) =>
+                          item.hasChildren ? (
+                            <li key={index} className="has-children">
+                              <Link onClick={() => setToggle(!toggle)} to={item.link}>
+                                {item.name}
+                              </Link>
+                              <ul className="sub-menu">
+                                {item.children.map((child, index) => (
+                                  <li key={index}>
+                                    <Link onClick={() => setToggle(!toggle)} to={child.link}>
+                                      {child.name}
+                                    </Link>
+                                  </li>
+                                ))}
+                              </ul>
                             </li>
-                          ))}
-                        </ul>
-                      </li>
-                    ) : (
-                      <li key={index}>
-                        <Link to={item.link}>{item.name}</Link>
-                      </li>
-                    )
-                  )}
-                </ul>
-              </nav>
-            </div>
-            <div className="header-right">
-              <div className="d-none d-sm-inline-block">
-                <Link className="btn btn-brand-1 hover-up" to="/contact">
-                  Request Demo
-                </Link>
+                          ) : (
+                            <li key={index}>
+                              <Link onClick={() => setToggle(!toggle)} to={item.link}>
+                                {item.name}
+                              </Link>
+                            </li>
+                          )
+                        )}
+                      </ul>
+                    </nav>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
