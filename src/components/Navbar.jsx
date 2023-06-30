ba91764788fdfea2cf0717b45b67a572a4db4551
kallyas <kallyasmedia@gmail.com>
2023-06-30 08:46:13 +0300

    fix: mobile menu state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(!false);
+  const [toggle, setToggle] = useState(false);
   const menu = [
     {
       name: "Home",
