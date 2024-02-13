a19b29e8acca4248fed758310544b5eeecfafc87
kallyas <kallyasmedia@gmail.com>
2024-02-13 14:51:37 +0300

    Refactor CardPolicyPage component and add pre-order terms and conditions

--- src/pages/card-policy.jsx
+++ src/pages/card-policy.jsx
@@ -1,10 +1,122 @@
-import React from 'react'
-import PDFViewer from '../components/PDFViewer'
+import React from "react";
+import "./terms.css";
 
 const CardPolicyPage = () => {
   return (
-    <PDFViewer pdf="./legal/card-policy.pdf" />
-  )
-}
+    <div className="section">
+      <div className="section">
+        <div className="container">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-lg-10" style={{ maxWidth: "800px" }}>
+              <h4 className="text-center mb-3 mt-3">
+                Pre-Order Terms and Conditions for Qiribu Card
+              </h4>
+              <p>Effective Date: [01.01.2024]</p>
+              <p>Thank you!</p>
+              <p>
+                Thank you for your interest in pre-ordering the Qiribu Card, a better way to get
+                paid! The Qiribu Card is designed to give you control of your salary and finances,
+                by offering a range of innovative features and benefits. Please read the following
+                terms and conditions carefully before proceeding with your pre-order:
+              </p>
+              <p>
+                <strong>About the Qiribu Card:</strong>
+              </p>
+              <p>
+                The Qiribu Card is a financial product that combines convenience, security, and
+                flexibility. With features such as zero-fee cashouts, customizable spending limits,
+                autosaving and robust security measures, the Qiribu Card aims to redefine the way
+                you manage your finances.
+              </p>
+              <p>
+                <strong>Pre-Order Information:</strong>
+              </p>
+              <p>
+                By pre-ordering the Qiribu Card, you acknowledge and agree that you are expressing
+                your interest in obtaining the Qiribu Card once it becomes available for issuance.
+                Pre-ordering does not guarantee immediate access to the card or its services.
+              </p>
+              <p>
+                <strong>Card Services Availability:</strong>
+              </p>
+              <p>
+                You understand and acknowledge that the Qiribu Card services are currently under
+                development and not yet available for use. We are working diligently to finalize the
+                card services and will notify you once they are ready for activation.
+              </p>
+              <p>
+                <strong>Consent to Share Personal Information:</strong>
+              </p>
+              <p>
+                By pre-ordering the Qiribu Card, you consent to Qiribu sharing your personal
+                information with our banking partner. This information will be used solely for the
+                purpose of demonstrating demand for the cards and facilitating the issuance process.
+                Your personal information will be handled in accordance with our Privacy Policy.
+              </p>
+              <p>
+                <strong>No Obligation to Purchase:</strong>
+              </p>
+              <p>
+                Pre-ordering the Qiribu Card does not obligate you to purchase the card or use its
+                services once they become available. You may cancel your pre-order at any time
+                before the card services are activated without any penalty or obligation.
+              </p>
+              <p>
+                <strong>Communication Regarding Pre-Order:</strong>
+              </p>
+              <p>
+                We will keep you informed about the progress of the Qiribu Card services and provide
+                updates on the estimated timeline for availability. You may receive communication
+                via email or other channels provided during the pre-order process.
+              </p>
+              <p>
+                <strong>Changes and Updates:</strong>
+              </p>
+              <p>
+                Qiribu reserves the right to make changes or updates to these pre-order terms and
+                conditions at any time without prior notice. Any modifications will be effective
+                immediately upon posting on our website or other communication channels.
+              </p>
+              <p>
+                <strong>Limitation of Liability:</strong>
+              </p>
+              <p>
+                Qiribu shall not be liable for any damages, losses, or inconvenience arising from
+                the unavailability of the Qiribu Card services during the pre-order period. Your
+                pre-order is voluntary and at your own risk.
+              </p>
+              <p>
+                <strong>Contact Us:</strong>
+              </p>
+              <p>
+                These pre-order terms and conditions shall be governed by and construed in
+                accordance with the laws of Uganda. Any disputes arising out of or
+                relating to these terms and conditions shall be subject to the exclusive
+                jurisdiction of the courts in Uganda.
+              </p>
+              <p>
+                <strong>Contact Us:</strong>
+              </p>
+              <p>
+                If you have any questions or concerns, please contact us at
+                <a className="mx-2" href="mailto:support@qiribu.com">
+                  support@qiribu.com
+                </a>
+              </p>
+              <p>
+                By proceeding with your pre-order of the Qiribu Card, you indicate your acceptance
+                of these terms and conditions.
+              </p>
+              <p>
+                Thank you for choosing Qiribu! We look forward to supporting you to take control of
+                your financial health.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CardPolicyPage
+export default CardPolicyPage;
