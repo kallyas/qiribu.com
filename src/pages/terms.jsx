f6df63e49b9596c0eb438f956c7a712049b081ad
kallyas <kallyasmedia@gmail.com>
2024-02-13 08:41:08 +0300

    Add TermsPage component

---
+++ src/pages/terms.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PDFViewer from "../components/PDFViewer";
+
+const TermsPage = () => {
+  return <PDFViewer pdf="./legal/terms.pdf" />;
+};
+
+export default TermsPage;
