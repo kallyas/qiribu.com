2cce4bd2eb4db7afc141f3b289d76d513692ace7
kallyas <kallyasmedia@gmail.com>
2024-02-13 08:41:00 +0300

    Add privacy page with PDF viewer

---
+++ src/pages/privacy.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PDFViewer from '../components/PDFViewer'
+
+const PrivacyPage = () => {
+  return (
+    <PDFViewer pdf="./legal/privacy.pdf" />
+  )
+}
+
+export default PrivacyPage
