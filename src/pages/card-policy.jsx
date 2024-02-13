c508467483bc98a40bb1b9c9401d6e0b0b7d61c7
kallyas <kallyasmedia@gmail.com>
2024-02-13 08:40:50 +0300

    Add CardPolicyPage component

---
+++ src/pages/card-policy.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PDFViewer from '../components/PDFViewer'
+
+const CardPolicyPage = () => {
+  return (
+    <PDFViewer pdf="./legal/card-policy.pdf" />
+  )
+}
+
+export default CardPolicyPage
